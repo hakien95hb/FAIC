bfe2d26cda96086be88ecc5e905f4d56729b7e7a
hakien <hakien95hb@gmail.com>
2018-11-24 10:19:07 +0700

    First

---
+++ Exam2/Data/Exam2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Exam2.Models;
+
+namespace Exam2.Models
+{
+    public class Exam2Context : DbContext
+    {
+        public Exam2Context (DbContextOptions<Exam2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Exam2.Models.Category> Category { get; set; }
+
+        public DbSet<Exam2.Models.Product> Product { get; set; }
+    }
+}
---
+++ Exam2/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exam2.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID {get; set;}
+        public string ProductName {get; set;}
+        public int SupplierID {get; set;}
+        public int CategoryID {get; set;}
+        public string QuantilyPerUnit {get; set;}
+        public string UnitPrice {get; set;}
+        public string UnitslnStock {get; set;}
+        public string UnitOnOrder {get; set;}
+        public string ReorderLevel {get; set;}
+        
+        public string Discontinued {get; set;}
+        [ForeignKey("CategoryID")]
+        public Category Category { get; set; }
+       
+
+           
+}
+}
